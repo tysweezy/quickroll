96a1cfb8e480e8c5b2d9e621ae1ed7ef57b437bb
tysweezy <tsouza.sf@gmail.com>
2021-01-05 19:05:21 -0800

    constrain roll to dice list [WIP]

--- quickroll.py
+++ quickroll.py
@@ -1,16 +1,14 @@
-#!/usr/bin/python3
-
 from random import randint
 import click
 
-# dice_list = [20, 10, 6]
+dice_list = [20, 10, 6, 8]
 
 @click.command()
 @click.option('--d', type=int, default=20)
 def roll(d):
-    if d == 20:
-        num_roll = randint(1, 20)
-        click.echo(f'rolled: {num_roll}')
+    if d in dice_list:
+        num_roll = randint(1, d)
+        click.echo(f"Rolled: {num_roll}")
     else:
         raise Exception("Invalid dice type")
 
